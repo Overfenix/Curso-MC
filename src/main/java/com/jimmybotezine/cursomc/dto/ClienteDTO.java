03227089270f321104ffcce0f9b8d9015a4b4a16
Jimmy Botezine <jimmy.botezine@ntendencia.com.br>
2019-07-22 17:02:20 -0300

    Validacao customizada: email nao repetido na atualizacao do cliente

--- src/main/java/com/jimmybotezine/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/jimmybotezine/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.jimmybotezine.cursomc.domain.Cliente;
+import com.jimmybotezine.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
