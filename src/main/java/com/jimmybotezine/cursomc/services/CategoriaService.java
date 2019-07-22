f91cd271d7a6b6039eb6be7957ebdf03844282fa
Jimmy Botezine <jimmy.botezine@ntendencia.com.br>
2019-07-22 11:34:39 -0300

    Correcoes em CategoriaService e construtor Pagamento

--- src/main/java/com/jimmybotezine/cursomc/services/CategoriaService.java
+++ src/main/java/com/jimmybotezine/cursomc/services/CategoriaService.java
@@ -35,10 +35,11 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		try {
 			repo.delete(find(id));	
@@ -59,5 +60,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(),objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		
 	}
 }
