71aab530f9115f956eb9f28dc8e35b47bfea0a80
Jimmy Botezine <jimmy.botezine@ntendencia.com.br>
2019-07-25 15:20:46 -0300

    Implementando SmtpEmailService com servidor Google

--- src/main/java/com/jimmybotezine/cursomc/config/TestConfig.java
+++ src/main/java/com/jimmybotezine/cursomc/config/TestConfig.java
@@ -5,14 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.jimmybotezine.cursomc.services.EmailService;
-import com.jimmybotezine.cursomc.services.MockEmailService;
+import com.jimmybotezine.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
 	
+	/*
+	 * @Bean public EmailService emailService() { return new MockEmailService(); }
+	 */
+	
 	@Bean
 	public EmailService emailService() {
-		return new MockEmailService();
+		return new SmtpEmailService();
 	}
 }
